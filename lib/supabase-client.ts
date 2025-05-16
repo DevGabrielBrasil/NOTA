c674ab703af5bcfc4fc4dcb7d9dfeef1c37a18aa
DevGabrielBrasil <gabriel.f.wlt@gmail.com>
2025-05-16 19:58:54 -0300

    Mudando nome dos env

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -6,11 +6,11 @@ let supabaseClient: ReturnType<typeof supabaseCreateClient<Database>> | null = n
 export function createClient() {
   if (supabaseClient) return supabaseClient
 
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  const supabaseUrl = process.env.SUPABASE_URL
+  const supabaseKey = process.env.SUPABASE_ANON_KEY
 
   if (!supabaseUrl || !supabaseKey) {
-    throw new Error("Variáveis de ambiente NEXT_PUBLIC_SUPABASE_URL e NEXT_PUBLIC_SUPABASE_ANON_KEY são necessárias")
+    throw new Error("Variáveis de ambiente SUPABASE_URL e SUPABASE_ANON_KEY são necessárias")
   }
 
   supabaseClient = supabaseCreateClient<Database>(supabaseUrl, supabaseKey) as ReturnType<typeof supabaseCreateClient<Database>>
